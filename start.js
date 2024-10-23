18979337b42a97a374e3d8b54bdc547eb340c3a9
spelsinx <108368693+spelsinx@users.noreply.github.com>
2024-10-23 12:04:39 +0500

    Create start.js

---
+++ start.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Указываем текущий каталог как источник статических файлов
+app.use(express.static(path.join(__dirname)));
+
+// Роут для загрузки index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+module.exports = app;
